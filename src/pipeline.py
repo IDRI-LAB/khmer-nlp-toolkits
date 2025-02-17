739d1de3252f6177ae30d8f06fa2fd0ec91a3c91
Sopagna <sopagna.heang@cadt.edu.kh>
2025-02-18 01:44:51 +0700

    [Feature] pipeline class

---
+++ src/pipeline.py
@@ -0,0 +1,82 @@
+"""
+Pipeline class 
+"""
+from typing import Callable, Any
+
+
+class Pipeline:
+    def __init__(self):
+        self.steps = []
+        self.info = []
+
+    def __str__(self):
+        description = [
+            "==============================",
+            "All function flow in pipeline.",
+            "=============================="
+        ]
+        for i, element in enumerate(self.info, 1):
+            func_name, params = list(element.items())[0]
+            formatted_str = f"{i}. {func_name}(" + ", ".join(f"{key}={value}" for key, value in params.items()) + ")"
+            description.append(formatted_str)
+        return "\n".join(description) + "\n"
+
+    def add(self, step: Callable, **step_params):
+        """
+        Add a step with its associated parameters.
+
+        Parameters
+        ----------
+        step: Callable
+            A function is added to pipeline.
+        **step_params: Any
+            Any parameters are accepted by step.
+        """
+        if not callable(step):
+            raise TypeError(f"{step} is not a valid pipeline step (must be callable)")
+        wrapped_step = self.process_step(step, **step_params)
+        self.steps.append(wrapped_step)
+        self.info.append({step.__name__: step_params})
+
+    def process_step(self, func: Callable, **step_params):
+        """
+        Wraps any function to add a `.process()` method and includes custom parameters.
+        """
+        def wrapped(data: Any):
+            return func(data, **step_params)
+
+        wrapped.process = wrapped  # Adds the .process() method
+        return wrapped
+
+    def run(self, data: Any, **kwargs):
+        for step in self.steps:
+            data = step.process(data, **kwargs)  # Call .process() with the correct parameters
+        return data
+
+
+if __name__ == "__main__":
+    # Example custom function
+    def funcA(text: str, is_upper: bool = False) -> str:
+        """A function that conditionally capitalizes text."""
+        return text.upper() if is_upper else text.lower()
+
+    def funcB(text: str, times: int = 1) -> str:
+        """Repeat the text a specified number of times."""
+        return text * times
+    # Create the pipeline
+    pipeline = Pipeline()
+
+    # Add funcA to the pipeline with its parameters
+    pipeline.add(funcA, is_upper=True)  # Pass `is_upper=True` to funcA
+
+    # Add funcB to the pipeline with its parameters
+    pipeline.add(funcB, times=3)  # Pass `times=3` to funcB
+
+    # Run the pipeline with input text
+
+    print(str(pipeline))
+    input_text = "hello world"
+    output_text = pipeline.run(input_text)
+
+    print(output_text)  # Output: HELLO WORLDHELLO WORLDHELLO WORLD
+
