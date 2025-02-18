0a979173527f5e681f8026112261a6cf78c382bb
Sopagna <sopagna.heang@cadt.edu.kh>
2025-02-18 18:56:35 +0700

    [Feature] Update pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -8,6 +8,7 @@ class Pipeline:
     def __init__(self):
         self.steps = []
         self.info = []
+        self.desc = []
 
     def __str__(self):
         description = [
@@ -15,13 +16,18 @@ class Pipeline:
             "All function flow in pipeline.",
             "=============================="
         ]
-        for i, element in enumerate(self.info, 1):
+        for i, (element, func_desc) in enumerate(zip(self.info, self.desc), 1):
             func_name, params = list(element.items())[0]
             formatted_str = f"{i}. {func_name}(" + ", ".join(f"{key}={value}" for key, value in params.items()) + ")"
             description.append(formatted_str)
+            if func_desc is not None:
+                description.append("\t" + func_desc)
         return "\n".join(description) + "\n"
+    
+    def __len__(self):
+        return len(self.info)
 
-    def add(self, step: Callable, **step_params):
+    def add(self, step: Callable, desc: str = None, **step_params):
         """
         Add a step with its associated parameters.
 
@@ -37,6 +43,7 @@ class Pipeline:
         wrapped_step = self.process_step(step, **step_params)
         self.steps.append(wrapped_step)
         self.info.append({step.__name__: step_params})
+        self.desc.append(desc)
 
     def process_step(self, func: Callable, **step_params):
         """
@@ -63,20 +70,19 @@ if __name__ == "__main__":
     def funcB(text: str, times: int = 1) -> str:
         """Repeat the text a specified number of times."""
         return text * times
+
     # Create the pipeline
     pipeline = Pipeline()
 
-    # Add funcA to the pipeline with its parameters
-    pipeline.add(funcA, is_upper=True)  # Pass `is_upper=True` to funcA
-
-    # Add funcB to the pipeline with its parameters
+    # Add funcA and funcB to the pipeline with its parameters
+    pipeline.add(funcA, is_upper=True, desc="lower or upper text.")  # Pass `is_upper=True` to funcA
     pipeline.add(funcB, times=3)  # Pass `times=3` to funcB
 
-    # Run the pipeline with input text
-
+    # List function added to pipeline.
     print(str(pipeline))
+
+    # Run the pipeline with input text
     input_text = "hello world"
     output_text = pipeline.run(input_text)
-
-    print(output_text)  # Output: HELLO WORLDHELLO WORLDHELLO WORLD
+    print(output_text)
 
