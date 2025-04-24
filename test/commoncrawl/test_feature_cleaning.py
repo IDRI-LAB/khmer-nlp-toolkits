940775e7481b4bba2d4e59dd658fa0086884be63
Sopagna <sopagna.heang@cadt.edu.kh>
2025-04-24 14:55:34 +0700

    [test] update test

--- test/commoncrawl/test_feature_cleaning.py
+++ test/commoncrawl/test_feature_cleaning.py
@@ -6,15 +6,13 @@ import pytest
 """
     clean text data and remove sentences that are not in Khmer
 """
-
-
 @pytest.mark.parametrize('exp_input, exp_output', [
     (
         [
             "លោក តាន់ ហ្វ្រង់ស្វ័រ៖ (Video inside)\nSearch ...\nFRESH NEWS+ Chinese (中文)\nលោក តាន់\n2024-06-19 07:08pm",
             [
-                {'label': 'km', 'prob': 0.99024457}, {'label': 'En', 'prob': 0.923842}, None, {
-                    'label': 'km', 'prob': 0.99024457}, None
+                {'label': 'km', 'prob': 0.99024457}, {'label': 'En', 'prob': 0.923842}, None, 
+                {'label': 'km', 'prob': 0.99024457}, None
             ]
         ],
         [
@@ -24,8 +22,24 @@ import pytest
     )
 ]
 )
-def test_filter_data(exp_input, exp_output):
-    content, sent_iden = exp_input
-    assert len(content.split('\n')) == len(sent_iden)
+def test_filter_data_equal_len(exp_input, exp_output):
     output = filter_kh_lng(*exp_input)
     assert output == exp_output
+
+
+@pytest.mark.parametrize('exp_input, exp_output', [
+    (
+        [
+            "លោក តាន់ ហ្វ្រង់ស្វ័រ៖ (Video inside)\nSearch ...\nFRESH NEWS+ Chinese (中文)\nលោក តាន់\n2024-06-19 07:08pm",
+            [
+                {'label': 'km', 'prob': 0.99024457}, {'label': 'En', 'prob': 0.923842}, None, 
+                {'label': 'km', 'prob': 0.99024457}, None, {'label': 'km', 'prob': 0.99024457}
+            ]
+        ],
+        None
+    )
+]
+)
+def test_filter_data_unequal_len(exp_input, exp_output):
+    with pytest.raises(ValueError, match="List is not in equal lenght"):
+        filter_kh_lng(*exp_input)
