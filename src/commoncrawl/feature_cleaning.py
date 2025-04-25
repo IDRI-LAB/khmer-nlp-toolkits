08c23512a480a1e1779d526f2660a6f01f9f4339
ChilyRan <chilyran@gmail.com>
2025-04-25 16:34:34 +0700

    [feature] check quality warning

--- src/commoncrawl/feature_cleaning.py
+++ src/commoncrawl/feature_cleaning.py
@@ -17,6 +17,7 @@ def cleaning_kh_data(data: Union[dict, list]):
     """
 
     cleaned_sents = filter_kh_lng(data['content'], data['metadata']['sentence_identifications'])
+    cleaned_sents = check_quality_warning(cleaned_sents, data['metadata']['quality_warnings'])
     return cleaned_sents
 
 
@@ -24,6 +25,7 @@ def filter_kh_lng(contents: List[str], sent_idens: List[Optional[Dict[str, float
     """
         Filter data to get only the content with Khmer language label ("kh")
         and Identification data is not None.
+        Parameters
         ===================
         contents: List[str]
                   content for cleaning
@@ -39,3 +41,61 @@ def filter_kh_lng(contents: List[str], sent_idens: List[Optional[Dict[str, float
         if iden_data is not None and iden_data['label'] == "km":
             list_data_kh.append(con_data)
     return list_data_kh
+
+
+def check_quality_warning(sentences: List[str], qua_warning: List[str], threshold: int = 75):
+    """
+    check quality warninng data.
+
+    Parameters
+    ==========
+    sentences: List[str]
+        list of sentence for cleaning if it has quality warning
+    qua_warning: List[str]
+        list of quality warning
+    threshold: int = 75
+        threshold to filter sentence character length
+
+    Return
+    ======
+    sentences: List[str]
+        list of sentence for cleaning if it has quality warning
+
+    Noted
+    ======
+    - if sentence has number char more than 50% of the sentence, it will be removed.
+    - if sentence length is less than threshold, it will be removed.
+    """
+    # print("sentences", len(sentences))
+
+    def extract_numbers(sent: str):
+        """
+            extract sentence that has number char
+            Parameters
+            ===============================
+            sent: str
+                sentence to get only number char
+            EX: "សួស្ដី១២៣៤" -> "១២៣៤"
+        """
+        return ''.join(char for char in sent if char.isdigit())
+
+    if sentences and qua_warning:
+        print("sentences::", sentences)
+        return [
+            sent.replace('\u200b', '').strip()
+            for sent in sentences
+            if len(sent) > threshold and len(extract_numbers(sent)) / len(sent) < 0.5
+        ]
+    return sentences
+
+
+def remove_sentence_deduplicate(sentences: List[str], tlsh: str):
+    """
+        check duplicate sentence by tlsh value
+        Parameters
+        ==========================
+        sentences: List[str]
+                   list of sentence for cleaning
+        tlsh: str
+            tlsh value to check duplicate sentence
+    """
