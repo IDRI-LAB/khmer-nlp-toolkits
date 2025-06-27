313536e41d5ecb1aee1c57b39d92ab5f5c985bb2
ChilyRan <chilyran@gmail.com>
2025-06-27 14:10:39 +0700

    [feature] build pipline for all main feature

--- src/commoncrawl/feature_cleaning.py
+++ src/commoncrawl/feature_cleaning.py
@@ -8,6 +8,7 @@ from src.keywords import ALDULT_KW
 
 ADULT_URL_FILTER = re.compile(rf"(?:{'|'.join(re.escape(k) for k in ALDULT_KW)})", re.IGNORECASE)
 
+
 def cleaning_kh_data(data: Union[dict, list], threshold: int = 75) -> List[str]:
     """
     Cleaning all khmer data.
