8c4e32d4c0c9fc689515c6b8b84778a7eda43480
heangs <heangs@cale.imag.fr>
2026-01-18 20:49:59 +0100

    [fix] setup with model file

--- setup.py
+++ setup.py
@@ -4,4 +4,11 @@ setup(
     name="khmer_nlp_toolkits",
     version="0.1.0",
     packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "khmer_nlp_toolkits": [
+            "khmer_nlp_toolkits/utils/segment/model/*.bin"
+        ],
+    },
+
 )
