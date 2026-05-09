71ca99fc4fc95369a78fc5a4cb51b3a13c62dc66
pagna <sopagnaheang@gmail.com>
2026-05-09 07:26:59 +0000

    [Update] Quality warning with test

--- khmer_nlp_toolkits/commoncrawl/quality_warning.py
+++ khmer_nlp_toolkits/commoncrawl/quality_warning.py
@@ -1,52 +1,107 @@
+"""
+Web crawling context quality checking.
+"""
 import regex
 
 
-def is_tiny(context: str):
+__all__ = ["check_quality_warning"]
+
+
+def _is_tiny(context_line: list[str]):
     """
     Check if the context less than 5 sentences.
     """
-    if not isinstance(context, str):
-        raise TypeError("context must be a str.")
-    return True if len(context.split("\n")) < 5 else False
+    if not isinstance(context_line, list):
+        raise TypeError("context must be a list[str].")
+    return len(context_line) < 5
 
 
-def is_noisy(context: str):
+def _is_noisy(context: str):
     """
     Check if the context contain proportion of Khmer character compare to overall character
     is smaller than 0.5 ratio then it identifies as noisy.
     """
     if not isinstance(context, str):
         raise TypeError("context must be a str.")
-    return True if len(regex.findall(r"\p{khmer}", context))/len(context) < 0.5 else False
+    return len(regex.findall(r"\p{khmer}", context))/len(context) < 0.5
 
 
-def is_short_sentences(context: str):
+def _is_short_sentences(context_line: list[str]):
     """
-    Check if the context contain more short sentences (less than 75 character lenght)
+    Check if the context contain more short sentences (less than 100 character lenght)
     that exceed 0.5 ratio compare to total sentences.
     """
-    if not isinstance(context, str):
+    if not isinstance(context_line, list):
         raise TypeError("context must be a str.")
     count = 0
-    sentences = context.split("\n")
-    for sentence in sentences:
-        count += 1 if len(sentence) < 75 else 0
-    return True if count/len(sentences) > 0.5 else False
+    for sentence in context_line:
+        count += 1 if len(sentence) < 100 else 0
+    return count/len(context_line) >= 0.5
+
+
+def _is_header(context_line: list[str]):
+    """
+    Check if the top 20% lines of document are short_sentence,
+    then annotate it as header.
+    """
+    if not isinstance(context_line, list):
+        raise TypeError("context_line must be a list[str].")
+
+    bound = int(len(context_line) * 0.2) + 1
+    return _is_short_sentences(context_line[:bound])
+
+
+def _is_footer(context_line: list[str]):
+    """
+    Check if the bottom 20% lines of document are short_sentence,
+    then annotate it as footer.
+    """
+    if not isinstance(context_line, list):
+        raise TypeError("context_line must be a list[str].")
+
+    bound = int(len(context_line) * 0.2) + 1
+    return _is_short_sentences(context_line[-bound:])
 
 
 def check_quality_warning(context: str):
     """
     Get all quality warning from the context.
+
+    Parameters
+    ==========
+    context: str
+        Text for check.
+
+    Return
+    ======
+    list
+        List of warning flags.
+
+    Noted
+    =====
+    - noisy: Ratio of Khmer_char/total_char > 0.5
+    - tiny: Contain less than or equal to 5 lines
+    - short_sentence: count of line with less then 100 char/total lines > 0.5
+    - header: top 20% of lines are short_sentence
+    - footer: bottom 20% of lines is short_sentence
     """
     if not isinstance(context, str):
         raise TypeError("context must be a str.")
-    if not len(context):
-        None
+    if not context:
+        raise ValueError("String is empty.")
+
+    lines = context.split("\n")
+
     result = []
-    if is_tiny(context):
-        result.append("tiny")
-    if is_noisy(context):
+    if _is_noisy(context):
         result.append("noisy")
-    if is_short_sentences(context):
+    if _is_tiny(lines):
+        result.append("tiny")
+    if _is_short_sentences(lines):
         result.append("short_sentences")
+    if _is_header(lines):
+        result.append("header")
+    if _is_footer(lines):
+        result.append("footer")
+
     return result if result else None
